faf55048eb0c809fd033939f388f1867e2067d80
Scott McCauley <scott@ascottmccauley.com>
2013-04-20 14:31:44 -0400

    Initial Commit

---
+++ content/mu-plugins/load.php
@@ -0,0 +1,5 @@
+<?php // Load Must Use Plugins 
+require WPMU_PLUGIN_DIR . '/wp-enable-media-replace/enable-media-replace.php';
+require WPMU_PLUGIN_DIR . '/wp-regenerate-thumbnails/regenerate-thumbnails.php';
+require WPMU_PLUGIN_DIR . '/wp-db-backup/wp-db-backup.php';
+require WPMU_PLUGIN_DIR . '/wp-post-formats/cf-post-formats.php';
